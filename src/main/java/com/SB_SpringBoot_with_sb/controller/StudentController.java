8e4a2e047065830613d750daae487601a189fda2
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2024-07-29 21:10:58 +0530

    building the first sample rest api

--- src/main/java/com/SB_SpringBoot_with_sb/controller/StudentController.java
+++ src/main/java/com/SB_SpringBoot_with_sb/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.SB_SpringBoot_with_sb.controller;
 
+import com.SB_SpringBoot_with_sb.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -16,7 +18,26 @@ public class StudentController {
        "Subhash",
        "Birajdar"
      );
+ }
 
+ @GetMapping("/getAll") // http://localhost:8080/api/v1/students/getAll
+ public List<Student> findAllStudentThroughModelClass(){
+     return List.of(
+       new Student(
+               "shubham",
+               "Patil",
+               LocalDate.now(),
+               "spatil@gmail.com",
+               21
+       ) ,
+             new Student(
+                     "raj",
+                     "Patil",
+                     LocalDate.now(),
+                     "rajpatil@gmail.com",
+                     29
+             )
+     );
  }
 
 }
